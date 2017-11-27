cf5483efb68f2b30488511c69eaeb958ebbe14c1
Joao Dionisio <jd1583@hotmail.com>
2017-11-27 19:59:31 -0300

    Commit default000

--- app/src/main/java/com/example/goddionisio/aula05menu_02/MainActivity.java
+++ app/src/main/java/com/example/goddionisio/aula05menu_02/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.goddionisio.aula05menu_02;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.botao);
+        btn.setOnCreateContextMenuListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void onGroupItemClick (MenuItem item){
+        if(item.isChecked()){
+            item.setChecked(false);
+        } else {
+            item.setChecked(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, item.getTitle(),Toast.LENGTH_LONG).show();
+        return true;
+        //return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Toast.makeText(this, item.getTitle(),Toast.LENGTH_LONG).show();
+        return true;
     }
 }
